60b12aaafd4251fb3afb4a46796d82d6c375d823
Vineet <vineet.hawal@tech.better.team>
2019-10-25 10:36:39 +0200

    android resumable fix

--- android/src/main/java/com/vinzscam/RNTusClientModule.java
+++ android/src/main/java/com/vinzscam/RNTusClientModule.java
@@ -1,8 +1,4 @@
-
 package com.vinzscam.rntusclient;
-
-import android.content.SharedPreferences;
-import android.view.animation.AnimationUtils;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -13,7 +9,6 @@ import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 
-import java.lang.Runnable;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -21,19 +16,17 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.UUID;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.atomic.AtomicBoolean;
 
-import io.tus.android.client.TusPreferencesURLStore;
+import io.tus.java.client.TusURLMemoryStore;
+import io.tus.java.client.TusUpload;
 import io.tus.java.client.ProtocolException;
 import io.tus.java.client.TusClient;
 import io.tus.java.client.TusExecutor;
-import io.tus.java.client.TusUpload;
 import io.tus.java.client.TusUploader;
 
 public class RNTusClientModule extends ReactContextBaseJavaModule {
@@ -43,24 +36,12 @@ public class RNTusClientModule extends ReactContextBaseJavaModule {
     private final String ON_PROGRESS = "onProgress";
 
     private final ReactApplicationContext reactContext;
-    private Map<String, TusRunnable> executorsMap;
-    private Map<String, WritableMap> progressMap;
+    private static Map<String, TusRunnable> executorsMap = new HashMap<String, TusRunnable>();
     private ExecutorService pool;
-    private Throttle mThrottle;
-
-    final Runnable rProgress;
 
     public RNTusClientModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
-        this.executorsMap = new HashMap<String, TusRunnable>();
-        this.progressMap = new HashMap<String, WritableMap>();
-        this.mThrottle = new Throttle(500);
-        this.rProgress = new Runnable() {
-            public void run() {
-                sendProgressEvents();
-            }
-        };
         pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
     }
 
@@ -72,123 +53,78 @@ public class RNTusClientModule extends ReactContextBaseJavaModule {
     @ReactMethod
     public void createUpload(String fileUrl, ReadableMap options, Callback callback) {
         String endpoint = options.getString("endpoint");
-
         int chunkSize = options.getInt("chunkSize");
         int requestPayloadSize = options.getInt("requestPayloadSize");
 
-        Map<String, Object> rawHeaders = options.getMap("headers").toHashMap();
-        Map<String, Object> rawMetadata = options.getMap("metadata").toHashMap();
-
-        Map<String, String> metadata = new HashMap<>();
-        for(String key: rawMetadata.keySet()) {
-            metadata.put(key, String.valueOf(rawMetadata.get(key)));
-        }
-        Map<String, String> headers = new HashMap<>();
-        for(String key: rawHeaders.keySet()) {
-            headers.put(key, String.valueOf(rawHeaders.get(key)));
-        }
-
-        File file = new File(fileUrl);
-        TusUpload upload = null;
         try {
-            upload = new TusUpload(file);
             String uploadId = UUID.randomUUID().toString();
-            TusRunnable executor = new TusRunnable(fileUrl, uploadId, endpoint, chunkSize, requestPayloadSize, metadata, headers);
-            this.executorsMap.put(uploadId, executor);
+            TusRunnable executor = new TusRunnable(fileUrl, chunkSize, requestPayloadSize, uploadId, endpoint);
+            executorsMap.put(uploadId, executor);
             callback.invoke(uploadId);
-        } catch(FileNotFoundException | MalformedURLException e) {
-            callback.invoke((Object)null, e.getMessage());
+        } catch (FileNotFoundException | MalformedURLException e) {
+            callback.invoke((Object) null, e.getMessage());
         }
     }
 
     @ReactMethod
-    public void resume(String uploadId, Callback callback) {
-        TusRunnable executor = this.executorsMap.get(uploadId);
-        if(executor != null) {
-            pool.submit(executor);
+    public void resume(String uploadId, String fileUrl, String endpoint, int chunkSize, int requestPayloadSize,
+            Callback callback) {
+        try {
+            TusRunnable existingExecutor = executorsMap.get(uploadId);
+            if (existingExecutor != null) {
+                existingExecutor.run();
+                return;
+            }
+            TusRunnable executor = new TusRunnable(fileUrl, chunkSize, requestPayloadSize, uploadId, endpoint);
+            executorsMap.put(uploadId, executor);
+            executor.run();
             callback.invoke(true);
-        } else {
+
+        } catch (FileNotFoundException | MalformedURLException e) {
             callback.invoke(false);
         }
+
     }
 
     @ReactMethod
     public void abort(String uploadId, Callback callback) {
         try {
-            TusRunnable executor = this.executorsMap.get(uploadId);
-            if(executor != null) {
+            TusRunnable executor = executorsMap.get(uploadId);
+            if (executor != null) {
                 executor.finish();
             }
-            callback.invoke((Object)null);
-        } catch(IOException | ProtocolException e) {
+            callback.invoke((Object) null);
+        } catch (IOException | ProtocolException e) {
             callback.invoke(e);
         }
-    }
-
-    public void sendProgressEvents() {
-        for (WritableMap params : this.progressMap.values()) {
-            this.reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-                    .emit(ON_PROGRESS, params);
-        }
-        this.progressMap.clear(); 
-    }
-    
-    class Throttle {
-        private long mLastFiredTimestamp;
-        private long mInterval;
-        
-        public Throttle(long interval) {
-            mInterval = interval;
-        }
-        
-        public void attempt(Runnable runnable) {
-            if (hasSatisfiedInterval()) {
-            runnable.run();
-            mLastFiredTimestamp = getNow();
-            }
-        }
-        
-        private boolean hasSatisfiedInterval() {
-            long elapsed = getNow() - mLastFiredTimestamp;
-            return elapsed >= mInterval;
-        }
-        
-        private long getNow() {
-            return AnimationUtils.currentAnimationTimeMillis();
-        }
-    
     }
 
     class TusRunnable extends TusExecutor implements Runnable {
         private TusUpload upload;
         private TusUploader uploader;
         private String uploadId;
-        private TusClient client;
-        private boolean shouldFinish;
-        private boolean isRunning;
         private int chunkSize;
         private int requestPayloadSize;
 
-        public TusRunnable(String fileUrl,
-                           String uploadId,
-                           String endpoint,
-                           int chunkSize, 
-                           int requestPayloadSize,
-                           Map<String, String> metadata,
-                           Map<String, String>headers
-        ) throws FileNotFoundException, MalformedURLException {
+        private TusClient client;
+        private Timer progressTicker;
+        private boolean shouldFinish;
+        private boolean isRunning;
+
+        public TusRunnable(String fileUrl, int chunkSize, int requestPayloadSize, String uploadId, String endpoint)
+                throws FileNotFoundException, MalformedURLException {
             this.uploadId = uploadId;
             this.chunkSize = chunkSize;
             this.requestPayloadSize = requestPayloadSize;
+
             client = new TusClient();
             client.setUploadCreationURL(new URL(endpoint));
 
-            SharedPreferences pref = getReactApplicationContext().getSharedPreferences("tus", 0);
-            client.enableResuming(new TusPreferencesURLStore(pref));
-            client.setHeaders(headers);
+            client.enableResuming(new TusURLMemoryStore());
+
             File file = new File(fileUrl);
-            upload = new TusUpload((file));
-            upload.setMetadata(metadata);
+            upload = new TusUpload(file);
+
             shouldFinish = false;
             isRunning = false;
         }
@@ -197,27 +133,43 @@ public class RNTusClientModule extends ReactContextBaseJavaModule {
             uploader = client.resumeOrCreateUpload(upload);
             uploader.setChunkSize(chunkSize);
             uploader.setRequestPayloadSize(requestPayloadSize);
-            do {
-                long totalBytes = upload.getSize();
-                long bytesUploaded = uploader.getOffset();
-                WritableMap params = Arguments.createMap();
-                params.putString("uploadId", uploadId);
-                params.putDouble("bytesWritten", bytesUploaded);
-                params.putDouble("bytesTotal", totalBytes);
-                progressMap.put(uploadId, params);
-                mThrottle.attempt(rProgress);
-            }while(uploader.uploadChunk() > -1 && !shouldFinish);
 
+            progressTicker = new Timer();
+
+            progressTicker.scheduleAtFixedRate(new TimerTask() {
+                @Override
+                public void run() {
+                    sendProgressEvent(upload.getSize(), uploader.getOffset());
+                }
+            }, 0, 500);
+
+            do {
+            } while (uploader.uploadChunk() > -1 && !shouldFinish);
+
+            progressTicker.cancel();
+            sendProgressEvent(upload.getSize(), upload.getSize());
             uploader.finish();
         }
 
+        private void sendProgressEvent(long bytesTotal, long bytesUploaded) {
+            WritableMap params = Arguments.createMap();
+
+            params.putString("uploadId", uploadId);
+            params.putDouble("bytesWritten", bytesUploaded);
+            params.putDouble("bytesTotal", bytesTotal);
+
+            reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit(ON_PROGRESS, params);
+        }
+
         public void finish() throws ProtocolException, IOException {
-            if(isRunning) {
+            if (isRunning) {
                 shouldFinish = true;
-            }
-            else {
-                if(uploader!= null){
+            } else {
+                if (uploader != null) {
                     uploader.finish();
+                }
+                if (progressTicker != null) {
+                    progressTicker.cancel();
                 }
             }
         }
@@ -232,14 +184,12 @@ public class RNTusClientModule extends ReactContextBaseJavaModule {
                 WritableMap params = Arguments.createMap();
                 params.putString("uploadId", uploadId);
                 params.putString("uploadUrl", uploadUrl);
-                reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-                        .emit(ON_SUCCESS, params);
+                reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit(ON_SUCCESS, params);
             } catch (ProtocolException | IOException e) {
                 WritableMap params = Arguments.createMap();
                 params.putString("uploadId", uploadId);
                 params.putString("error", e.toString());
-                reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-                        .emit(ON_ERROR, params);
+                reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit(ON_ERROR, params);
             }
             isRunning = false;
         }
--- dist/index.js
+++ dist/index.js
@@ -1,151 +1,191 @@
-import { NativeModules, NativeEventEmitter, Platform } from "react-native";
+import { NativeModules, NativeEventEmitter, Platform } from 'react-native';
 const { RNTusClient } = NativeModules;
 const tusEventEmitter = new NativeEventEmitter(RNTusClient);
 const defaultOptions = {
-    headers: {},
-    metadata: {}
+  headers: {},
+  metadata: {},
 };
+
 /** Class representing a tus upload */
 class Upload {
-    /**
-     *
-     * @param settings The options argument used to setup your tus upload.
-     */
-    constructor(options) {
-        this.subscriptions = [];
-        this.aborting = false;
-        this.options = Object.assign({}, defaultOptions, options);
-        if (this.options.uploadId) {
-            this.uploadId = this.options.uploadId;
-            delete this.options.uploadId;
-        }
+  /**
+   *
+   * @param settings The options argument used to setup your tus upload.
+   */
+  constructor (options) {
+    this.subscriptions = [];
+    this.aborting = false;
+    this.options = Object.assign({}, defaultOptions, options);
+    if (this.options.uploadId) {
+      this.uploadId = this.options.uploadId;
+      delete this.options.uploadId;
     }
-    /**
-     * Start or resume the upload using the specified file.
-     * If no file property is available the error handler will be called.
-     */
-    start() {
-        this.aborting = false;
-        if (!this.options.file && !this.uploadId) {
-            this.emitError(new Error("tus: no file or stream to upload provided"));
-            return;
-        }
-        if (!this.options.endpoint) {
-            this.emitError(new Error("tus: no endpoint provided"));
-            return;
-        }
-        (this.uploadId ? Promise.resolve() : this.createUpload())
-            .then(() => this.resume())
-            .catch(e => this.emitError(e));
+  }
+
+  /**
+   * Start or resume the upload using the specified file.
+   * If no file property is available the error handler will be called.
+   */
+  start () {
+    this.aborting = false;
+    if (!this.options.file && !this.uploadId) {
+      this.emitError(new Error('tus: no file or stream to upload provided'));
+      return;
     }
-    /**
-     * Abort the currently running upload request and don't continue.
-     * You can resume the upload by calling the start method again.
-     */
-    abort() {
-        if (this.uploadId) {
-            this.aborting = true;
-            if (Platform.OS === "ios") {
-                RNTusClient.abort(this.uploadId, this.options.endpoint, this.options.chunkSize, (err) => {
-                    if (err) {
-                        this.emitError(err);
-                    }
-                });
+    if (!this.options.endpoint) {
+      this.emitError(new Error('tus: no endpoint provided'));
+      return;
+    }
+    return (this.uploadId ? Promise.resolve() : this.createUpload())
+      .then(() => this.resume())
+      .then(() => this.uploadId)
+      .catch(e => this.emitError(e));
+  }
+
+  /**
+   * Abort the currently running upload request and don't continue.
+   * You can resume the upload by calling the start method again.
+   */
+  abort () {
+    if (this.uploadId) {
+      this.aborting = true;
+      if (Platform.OS === 'ios') {
+        RNTusClient.abort(
+          this.uploadId,
+          this.options.endpoint,
+          this.options.chunkSize,
+          err => {
+            if (err) {
+              this.emitError(err);
             }
-            else {
-                RNTusClient.abort(this.uploadId, (err) => {
-                    if (err) {
-                        this.emitError(err);
-                    }
-                });
-            }
-        }
-    }
-    resume() {
-        if (Platform.OS === "ios") {
-            RNTusClient.resume(this.uploadId, this.options.endpoint, this.options.chunkSize, (hasBeenResumed) => {
-                if (!hasBeenResumed) {
-                    this.emitError(new Error("Error while resuming the upload"));
-                }
-            });
-        }
-        else {
-            RNTusClient.resume(this.uploadId, (hasBeenResumed) => {
-                if (!hasBeenResumed) {
-                    this.emitError(new Error("Error while resuming the upload"));
-                }
-            });
-        }
-        if (!this.subscriptions.length) {
-            this.subscribe();
-        }
-    }
-    emitError(error) {
-        if (this.options.onError) {
-            this.options.onError(error);
-        }
-        else {
-            throw error;
-        }
-    }
-    createUpload() {
-        return new Promise((resolve, reject) => {
-            const { file, metadata, headers, endpoint, chunkSize, requestPayloadSize } = this.options;
-            const settings = {
-                metadata,
-                headers,
-                endpoint,
-                chunkSize,
-                requestPayloadSize
-            };
-            RNTusClient.createUpload(file, settings, (uploadId, errorMessage) => {
-                this.uploadId = uploadId;
-                if (uploadId == null) {
-                    const error = errorMessage ? new Error(errorMessage) : null;
-                    reject(error);
-                }
-                else {
-                    this.subscribe();
-                    resolve();
-                }
-            });
+          }
+        );
+      } else {
+        RNTusClient.abort(this.uploadId, err => {
+          if (err) {
+            this.emitError(err);
+          }
         });
+      }
     }
-    subscribe() {
-        this.subscriptions.push(tusEventEmitter.addListener("onSuccess", payload => {
-            if (payload.uploadId === this.uploadId) {
-                this.url = payload.uploadUrl;
-                if (!this.aborting) {
-                    this.onSuccess();
-                    this.aborting = false;
-                }
-                this.unsubscribe();
-            }
-        }));
-        this.subscriptions.push(tusEventEmitter.addListener("onError", payload => {
-            if (payload.uploadId === this.uploadId && !this.aborting) {
-                this.onError(payload.error);
-            }
-        }));
-        this.subscriptions.push(tusEventEmitter.addListener("onProgress", payload => {
-            if (payload.uploadId === this.uploadId && !this.aborting) {
-                this.onProgress(payload.bytesWritten, payload.bytesTotal);
-            }
-        }));
+  }
+
+  resume () {
+    if (Platform.OS === 'ios') {
+      RNTusClient.resume(
+        this.uploadId,
+        this.options.endpoint,
+        this.options.chunkSize,
+        hasBeenResumed => {
+          if (!hasBeenResumed) {
+            this.emitError(new Error('Error while resuming the upload'));
+          }
+        }
+      );
+    } else {
+      RNTusClient.resume(
+        this.uploadId,
+        this.options.file,
+        this.options.endpoint,
+        this.options.chunkSize,
+        this.options.requestPayloadSize,
+        hasBeenResumed => {
+          if (!hasBeenResumed) {
+            this.emitError(new Error('Error while resuming the upload'));
+          }
+        }
+      );
     }
-    unsubscribe() {
-        this.subscriptions.forEach(subscription => subscription.remove());
+    if (!this.subscriptions.length) {
+      this.subscribe();
     }
-    onSuccess() {
-        this.options.onSuccess && this.options.onSuccess();
+  }
+
+  emitError (error) {
+    if (this.options.onError) {
+      this.options.onError(error);
+    } else {
+      throw error;
     }
-    onProgress(bytesUploaded, bytesTotal) {
-        this.options.onProgress &&
-            this.options.onProgress(bytesUploaded, bytesTotal);
-    }
-    onError(error) {
-        this.options.onError && this.options.onError(error);
-    }
+  }
+
+  createUpload () {
+    return new Promise((resolve, reject) => {
+      const {
+        file,
+        metadata,
+        headers,
+        endpoint,
+        chunkSize,
+        requestPayloadSize,
+      } = this.options;
+      const settings = {
+        metadata,
+        headers,
+        endpoint,
+        chunkSize,
+        requestPayloadSize,
+      };
+
+      RNTusClient.createUpload(file, settings, (uploadId, errorMessage) => {
+        this.uploadId = uploadId;
+        if (uploadId == null) {
+          const error = errorMessage ? new Error(errorMessage) : null;
+
+          reject(error);
+        } else {
+          resolve();
+          this.subscribe();
+        }
+      });
+    });
+  }
+
+  subscribe () {
+    this.subscriptions.push(
+      tusEventEmitter.addListener('onSuccess', payload => {
+        if (payload.uploadId === this.uploadId) {
+          this.url = payload.uploadUrl;
+          if (!this.aborting) {
+            this.onSuccess();
+            this.aborting = false;
+          }
+          this.unsubscribe();
+        }
+      })
+    );
+    this.subscriptions.push(
+      tusEventEmitter.addListener('onError', payload => {
+        if (payload.uploadId === this.uploadId && !this.aborting) {
+          this.onError(payload.error);
+        }
+      })
+    );
+    this.subscriptions.push(
+      tusEventEmitter.addListener('onProgress', payload => {
+        if (payload.uploadId === this.uploadId && !this.aborting) {
+          this.onProgress(payload.bytesWritten, payload.bytesTotal);
+        }
+      })
+    );
+  }
+
+  unsubscribe () {
+    this.subscriptions.forEach(subscription => subscription.remove());
+  }
+
+  onSuccess () {
+    this.options.onSuccess && this.options.onSuccess();
+  }
+
+  onProgress (bytesUploaded, bytesTotal) {
+    this.options.onProgress &&
+      this.options.onProgress(bytesUploaded, bytesTotal);
+  }
+
+  onError (error) {
+    this.options.onError && this.options.onError(error);
+  }
 }
 export { Upload };
-//# sourceMappingURL=index.js.map
+// # sourceMappingURL=index.js.map
